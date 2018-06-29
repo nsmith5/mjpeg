40854e3faf760d163948e8836d76c03430e38d89
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2018-06-29 11:25:45 -0700

    switch to package

--- mjpeg.go
+++ mjpeg.go
@@ -1,14 +1,10 @@
-package main
+package mjpeg
 
 import (
 	"image"
-	"image/color"
 	"image/jpeg"
 	"io"
-	"log"
-	"math/rand"
 	"net/http"
-	"time"
 )
 
 // An ImageStream is a function that, when called repeatedly, returns successive
@@ -18,8 +14,6 @@ type ImageStream func() image.Image
 func (s ImageStream) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "multipart/x-mixed-replace; boundary=frame")
 	for {
-		time.Sleep(time.Second)
-
 		_, err := io.WriteString(w, "\r\n--frame\r\nContent-Type: image/jpeg\r\n\r\n")
 		if err != nil {
 			break
@@ -35,23 +29,4 @@ func (s ImageStream) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 	}
-}
-
-func stream() image.Image {
-	img := image.NewGray(image.Rect(0, 0, 100, 100))
-	for i := 0; i < 100; i++ {
-		for j := 0; j < 100; j++ {
-			n := rand.Intn(256)
-			gray := color.Gray{uint8(n)}
-			img.SetGray(i, j, gray)
-		}
-	}
-	return img
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/", ImageStream(stream))
-	log.Fatal(http.ListenAndServe(":8080", mux))
-	return
 }
---
+++ mjpeg_test.go
@@ -0,0 +1,30 @@
+package mjpeg_test
+
+import (
+	"image"
+	"image/color"
+	"log"
+	"math/rand"
+	"net/http"
+
+	"code.nfsmith.ca/nsmith/mjpeg"
+)
+
+func Example() {
+
+	stream := func() image.Image {
+		img := image.NewGray(image.Rect(0, 0, 100, 100))
+		for i := 0; i < 100; i++ {
+			for j := 0; j < 100; j++ {
+				n := rand.Intn(256)
+				gray := color.Gray{uint8(n)}
+				img.SetGray(i, j, gray)
+			}
+		}
+		return img
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/stream", mjpeg.ImageStream(stream))
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
